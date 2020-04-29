6736226958a2bf8c7e657cf4d3cfcc5ebeb50386
Sergio <serperalca@gmail.com>
2020-04-29 17:53:46 +0200

    Roles Creados, Inicio de vistar Admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/users', 'AdminUsersController');
