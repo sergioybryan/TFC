c844fe680a2b5fdd1b87d352f621a03007dd8bc5
Sergio <serperalca@gmail.com>
2020-05-26 11:40:37 +0200

    realizando vista home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::get('/home', 'HomeController@index')->name('home');
+
 
 Route::resource('admin/productos', 'AdminProductosController');
 
